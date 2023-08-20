9de43bf9fd80941bcf6541928736be300e4ed2a8
Maleesha Gimmana <99645165+maleeshag@users.noreply.github.com>
2023-08-20 09:35:58 +0530

    Add files via upload

---
+++ Weka_java_Apriori_paramters/Weka_With_Java.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package weka_with_java;
+
+/**
+ *
+ * @author ASUS
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStream;
+import weka.core.Instances;
+import weka.associations.Apriori;
+import weka.associations.FPGrowth;
+
+public class Weka_With_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        Instances data = new Instances(new BufferedReader(new FileReader("dataset/supermarket.arff")));
+        
+        Apriori model =  new Apriori();     // for Apriori Algorithmn
+
+//        FPGrowth model = new FPGrowth();  //FPGrowth Algorithmn
+        
+        //changing the parameters
+        model.setDelta(0.04);
+        model.setNumRules(6);
+        model.setUpperBoundMinSupport(1);
+        model.setLowerBoundMinSupport(0.1);
+        
+        
+         
+        model.buildAssociations(data);
+        System.out.println(model);
+
+    }
+    
+}
